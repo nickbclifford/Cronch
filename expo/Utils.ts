004e32eeaa687f8ff399c23f00e17e2959c5d6dd
Nick Clifford <nick@nickclifford.me>
2019-02-01 22:34:40 -0600

    oops why was this not async/awaited (thank you TypeScript refactorings)

--- expo/Utils.ts
+++ expo/Utils.ts
@@ -8,21 +8,22 @@ export interface APIResponse<T> {
 	data: T | null;
 }
 
-export function fetchWithJwt<T>(route: string, options: RequestInit) {
+export async function fetchWithJwt<T>(route: string, options: RequestInit) {
 	// These will only be called when the user is logged in, so we can safely say that JWT will always be defined
-	return MyMICDS.auth.$.toPromise().then(jwt => {
-		const injectedOptions = Object.assign(options, {
-			headers: {
-				'Authorization': `Bearer ${jwt}`,
-				'Content-Type': 'application/json'
-			}
-		});
-		return fetch(Config.backendUrl + route, injectedOptions);
-	}).then<APIResponse<T>>(r => r.json()).then(res => {
-		if (typeof res.error === 'string') {
-			return Promise.reject(new Error(res.error));
-		} else {
-			return Promise.resolve(res.data!);
+	const jwt = await MyMICDS.auth.$.toPromise();
+	const injectedOptions = Object.assign(options, {
+		headers: {
+			'Authorization': `Bearer ${jwt}`,
+			'Content-Type': 'application/json'
 		}
 	});
+
+	const body = await fetch(Config.backendUrl + route, injectedOptions);
+	const res = await body.json();
+
+	if (typeof res.error === 'string') {
+		throw new Error(res.error);
+	} else {
+		return res.data! as APIResponse<T>;
+	}
 }
