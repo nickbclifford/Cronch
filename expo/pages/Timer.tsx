6154940356513f7e3f4d3c8e261ea8718c3ba254
AsianPsychoBoy <ziyang_99@outlook.com>
2019-02-02 14:21:51 -0600

    state

--- expo/pages/Timer.tsx
+++ expo/pages/Timer.tsx
@@ -9,12 +9,21 @@ import AssignmentContext, { AssignmentContextType } from '../common/AssignmentCo
 import Hamburger from '../components/Hamburger';
 
 export interface TimerState {
+	workTimeLeft: number,
+	breakTimeLeft: number,
+	cyclesLeft: number,
+	inBreak: boolean
 }
 
 let mockAssignment: {
 	canvasEvent: CanvasEvent,
-	timeLength: number,
+	recommendedTime: number,
 	done: boolean
+}
+
+let userPreferredCycles: {
+	work: number,
+	break: number
 }
 
 export default class Timer extends React.Component<NavigationScreenProps, TimerState> {
@@ -26,9 +35,10 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 		header: null
 	};
 
+	private intervalHandler: NodeJS.Timer;
+
 	constructor(props: any) {
 		super(props);
-		this.state = {};
 
 		mockAssignment = {
 			canvasEvent: JSON.parse(`{
@@ -59,9 +69,25 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
                 "desc": "hehexd",
                 "descPlaintext": "Our goals today are to find the arc length of a space curve and to find the curvature of a curve at a point on the curve.\\nArc Length (s)=\\nCurvature is the measure of how sharply a curve bends.\\nFormal Definition     We don't want to use the formal definition. :)\\n\\n"
             }`),
-			timeLength: 45 * 60,
+			recommendedTime: 45 * 60 * 1000,
 			done: false
+		};
+
+		userPreferredCycles = {
+			work: 20 * 60 * 1000,
+			break: 5 * 60 * 1000
 		}
+
+		this.state = {
+			workTimeLeft: userPreferredCycles.work,
+			breakTimeLeft: userPreferredCycles.break,
+			cyclesLeft: Math.floor(mockAssignment.recommendedTime / userPreferredCycles.work),
+			inBreak: false
+		};
+
+		this.intervalHandler = setInterval(() => {
+
+		}, 1000);
 	}
 
 	@bind
@@ -83,7 +109,9 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 				<Button title='- Cycle'/>
 				<Text>{mockAssignment.canvasEvent.title}</Text>
 
-				<Text></Text>
+				<Text>Work Timer: {this.state.workTimeLeft}</Text>
+				<Text>Break Timer: {this.state.breakTimeLeft}</Text>
+				<Text>Cycles Left: {this.state.cyclesLeft}</Text>
 			</SafeAreaView>
 		);
 	}
