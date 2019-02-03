039b38b32d6c7055191b94ec144a0c50c321dd04
AsianPsychoBoy <ziyang_99@outlook.com>
2019-02-02 22:05:29 -0600

    things

--- expo/pages/Timer.tsx
+++ expo/pages/Timer.tsx
@@ -1,17 +1,19 @@
 import { CanvasEvent } from '@mymicds/sdk';
 import bind from 'bind-decorator';
 import * as React from 'react';
-import { StyleSheet, Text, Alert } from 'react-native';
+import { Alert, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationScreenProps, SafeAreaView } from 'react-navigation';
 
+import { gyro$ } from '../common/PhoneAcrobatics';
 import Hamburger from '../components/Hamburger';
 
 export interface TimerState {
-	workTimeLeft: number,
-	breakTimeLeft: number,
-	onBreak: boolean,
-	paused: boolean,
+	workTimeLeft: number;
+	breakTimeLeft: number;
+	onBreak: boolean;
+	paused: boolean;
+	manual: boolean;
 }
 
 let mockAssignment: {
@@ -24,7 +26,7 @@ let mockAssignment: {
 let userPreferredCycles: {
 	work: number,
 	break: number
-}
+};
 
 export default class Timer extends React.Component<NavigationScreenProps, TimerState> {
 
@@ -32,7 +34,7 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 		header: null
 	};
 
-	private interval: NodeJS.Timer | null = null;
+	private interval = 0;
 
 	constructor(props: any) {
 		super(props);
@@ -75,45 +77,27 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 		userPreferredCycles = {
 			work: 0.1 * 60 * 1000,
 			break: 0.1 * 60 * 1000
-		}
+		};
 
 		this.state = {
 			workTimeLeft: userPreferredCycles.work,
 			breakTimeLeft: userPreferredCycles.break,
 			onBreak: false,
-			paused: true
+			paused: false,
+			manual: false
 		};
 	}
 
 	componentDidMount() {
 		this.interval = setInterval(() => {
-			if (this.state.onBreak) {
-				this.setState({
-					breakTimeLeft: this.state.breakTimeLeft - 1000
-				});
-
-				if (this.state.breakTimeLeft <= 0) {
-					this.setState({
-						breakTimeLeft: userPreferredCycles.break,
-						onBreak: false
-					});
-				}
-			} else {
-				this.setState({
-					workTimeLeft: this.state.workTimeLeft - 1000
-				});
-				if (this.state.workTimeLeft <= 0) {
-					this.setState({
-						workTimeLeft: userPreferredCycles.work,
-						onBreak: true
-					});
-				}
+			if (!this.state.paused && !this.state.onBreak) {
+				this.tick();
 			}
 		}, 1000);
 	}
 
 	componentWillUnmount() {
-		clearInterval(this.interval)
+		clearInterval(this.interval);
 	}
 
 	@bind
@@ -123,11 +107,45 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 
 	@bind
 	private addCustom() {
+		return;
 	}
 
 	@bind
 	private pause() {
+		this.setState({
+			paused: true
+		});
+	}
 
+	@bind
+	private toggleManual() {
+
+	}
+
+	@bind
+	tick() {
+		if (this.state.onBreak) {
+			this.setState({
+				breakTimeLeft: this.state.breakTimeLeft - 1000
+			});
+
+			if (this.state.breakTimeLeft <= 0) {
+				this.setState({
+					breakTimeLeft: userPreferredCycles.break,
+					onBreak: false
+				});
+			}
+		} else {
+			this.setState({
+				workTimeLeft: this.state.workTimeLeft - 1000
+			});
+			if (this.state.workTimeLeft <= 0) {
+				this.setState({
+					workTimeLeft: userPreferredCycles.work,
+					onBreak: true
+				});
+			}
+		}
 	}
 
 	render() {
@@ -145,6 +163,7 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 
 				<Text>Work Timer: {Math.floor(this.state.workTimeLeft / 60000)}:{Math.floor(this.state.workTimeLeft % 60000)}</Text>
 				<Text>Break Timer: {Math.floor(this.state.breakTimeLeft / 60000)}:{Math.floor(this.state.breakTimeLeft % 60000)}</Text>
+				<Text>{this.state.paused.toString()}</Text>
 			</SafeAreaView>
 		);
 	}
