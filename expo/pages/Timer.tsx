fca8e71e95b97a0f99a9af025795ef365690e463
Andrew Zhao <azhao@micds.org>
2019-02-03 23:43:30 -0600

    Getting assignments in timer

--- expo/pages/Timer.tsx
+++ expo/pages/Timer.tsx
@@ -5,6 +5,7 @@ import { Alert, Picker, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { NavigationScreenProps, SafeAreaView } from 'react-navigation';
 import { createTimeslot } from '../common/Timeslot';
+import DisplayAssignments from '../components/DisplayAssignments';
 
 import flipped$ from '../common/PhoneAcrobatics';
 import Hamburger from '../components/Hamburger';
@@ -16,14 +17,8 @@ export interface TimerState {
 	paused: boolean;
 	modeSelection: number;
 	flipped: boolean;
+	assignment: CanvasEvent;
 }
-
-let mockAssignment: {
-	canvasEvent: CanvasEvent,
-	// recommendedTime: number,
-	timeSpent: number,
-	done: boolean
-};
 
 export default class Timer extends React.Component<NavigationScreenProps, TimerState> {
 
@@ -43,41 +38,6 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 	constructor(props: any) {
 		super(props);
 
-		mockAssignment = {
-			// tslint:disable:max-line-length
-			canvasEvent: JSON.parse(`{
-                "_id": "event-assignment-5872379",
-                "canvas": true,
-                "user": "jcai",
-                "class": {
-                    "_id": null,
-                    "canvas": true,
-                    "user": "jcai",
-                    "name": "MA660",
-                    "teacher": {
-                        "_id": null,
-                        "prefix": "",
-                        "firstName": "",
-                        "lastName": ""
-                    },
-                    "type": "other",
-                    "block": "other",
-                    "color": "#34444F",
-                    "textDark": false
-                },
-                "title": "Notes Arc Length and Curvature",
-                "start": "2019-01-16T06:00:00.000Z",
-                "end": "2019-01-16T06:00:00.000Z",
-                "link": "https://micds.instructure.com/courses/1130489/assignments/5872379",
-                "checked": false,
-                "desc": "hehexd",
-                "descPlaintext": "Our goals today are to find the arc length of a space curve and to find the curvature of a curve at a point on the curve.\\nArc Length (s)=\\nCurvature is the measure of how sharply a curve bends.\\nFormal Definition     We don't want to use the formal definition. :)\\n\\n"
-            }`),
-			// recommendedTime: 0.2 * 60 * 1000,
-			timeSpent: 0,
-			done: false
-		};
-
 		this.userCycles = [{
 			work: 0.1 * 60 * 1000,
 			break: 0.1 * 60 * 1000
@@ -92,7 +52,8 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 			onBreak: false,
 			paused: true,
 			modeSelection: 0,
-			flipped: false
+			flipped: false,
+			assignment: this.props.navigation.getParam('assignment')
 		};
 	}
 
@@ -245,13 +206,26 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 	}
 
 	private recordTimeSlot() {
-		return createTimeslot(new Date(), mockAssignment.canvasEvent._id);
+		return createTimeslot(new Date(), this.state.assignment._id);
+	}
+
+	@bind
+	private navigateToAssignmentDetails(assignment: CanvasEvent) {
+		this.props.navigation.navigate('AssignmentDetails', { assignment });
 	}
 
 	render() {
 		return (
 			<SafeAreaView style={styles.safeArea}>
 				<Hamburger toggle={this.props.navigation.toggleDrawer} />
+				<DisplayAssignments
+				navigation={this.props.navigation}
+				assignments={[this.state.assignment]}
+				headers={true}
+				paddingLeft={8}
+				paddingRight={8}
+				onAssignmentClick={this.navigateToAssignmentDetails}
+				/>
 				<Button
 					title='Create Battle Plan'
 					onPress={this.navigateToBattlePlan}
@@ -271,7 +245,7 @@ export default class Timer extends React.Component<NavigationScreenProps, TimerS
 					)}
 					<Picker.Item key={-1} label='Manual Timer' value={-1}/>
 				</Picker>
-				<Text>{mockAssignment.canvasEvent.title}</Text>
+				<Text>{this.state.assignment.title}</Text>
 
 				<Text>Work Timer: {Math.floor(this.state.workTimeLeft / 60000)}:{Math.floor(this.state.workTimeLeft % 60000)}</Text>
 				<Text>Break Timer: {Math.floor(this.state.breakTimeLeft / 60000)}:{Math.floor(this.state.breakTimeLeft % 60000)}</Text>
