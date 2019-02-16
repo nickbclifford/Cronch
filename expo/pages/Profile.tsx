7a8924feebf19be3abc8b613f78cb9902eb0b03d
Michael Gira <contact@michaelgira.me>
2019-02-15 19:56:28 -0600

    Beautify profile component

--- expo/pages/Profile.tsx
+++ expo/pages/Profile.tsx
@@ -3,13 +3,14 @@ import { Formik, FormikProps } from 'formik';
 import { number } from 'prop-types';
 import * as React from 'react';
 import { Alert, Picker, StatusBar, StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, CheckBox } from 'react-native-elements';
 import { NavigationScreenProps, SafeAreaView } from 'react-navigation';
 
 import { alarmList } from '../common/Alarms';
 import MyMICDS from '../common/MyMICDS';
 import createNavigationOptions from '../common/NavigationOptionsFactory';
-import { components, PRIMARY, typography } from '../common/StyleGuide';
+import { components, NEUTRAL, PRIMARY, typography } from '../common/StyleGuide';
+import { nunito } from '../common/StyleGuide';
 import { changeUserInfo, getUser, User } from '../common/User';
 import { handleFieldChangeFactory } from '../common/Utils';
 import CronchyUser from '../components/CronchyUser';
@@ -106,83 +107,127 @@ export default class Profile extends React.Component<NavigationScreenProps, Prof
 	// 		// }
 	// }
 
-	render() {
-		return (
-			<SafeAreaView style={styles.safeArea}>
-				<StatusBar barStyle='light-content' backgroundColor={PRIMARY[500]} animated={true} />
-				<View style={styles.userInfo}>
-					<Text style={typography.h3}>Logged in as: {MyMICDS.auth.snapshot ? MyMICDS.auth.snapshot.user : ''}</Text>
-					<Button
-						title='Logout'
-						onPress={this.logout}
-						buttonStyle={components.buttonStyle}
-						titleStyle={components.buttonText}
-					/>
-				</View>
-
-				<Text style={typography.h1}>Profile Settings</Text>
-
-				<Formik
-					initialValues={settingsFormInitialValues}
-					onSubmit={this.saveSettings}
-				>
-					{props => (
-						<View style={styles.container}>
-
-							<Question
-								question='How should we handle your timer data?'
-								responses={this.questionNames}
-								onSelectResponse={handleFieldChangeFactory<SettingsFormValues>(props, 'dataSharingSelection')}
-								selectedIndex={props.values.dataSharingSelection}
-							/>
-							<View style={styles.alarmPicker}>
-								<Text style={[typography.h3, styles.alarmPickerLabel]}>Alarm Preferences</Text>
-								<Picker
-									selectedValue={props.values.alarmSelection}
-									onValueChange={handleFieldChangeFactory<SettingsFormValues>(props, 'alarmSelection')}
-								>
-									{alarmList.map((alarm, i) =>
-										<Picker.Item
-											key={i}
-											label={alarm.displayName}
-											value={i}
-										/>
-									)}
-								</Picker>
-							</View>
-
-							<Button
-								title='Save'
-								onPress={props.handleSubmit as any}
-								buttonStyle={components.buttonStyle}
-								titleStyle={components.buttonText}
-							/>
-						</View>
-					)}
-				</Formik>
-				{/* Logout button for debug */}
-				<Button
-					title='Logout'
-					onPress={this.logout}
-					buttonStyle={components.buttonStyle}
-					titleStyle={components.buttonText}
-				/>
-			</SafeAreaView>
-		);
-	}
-
 	// render() {
 	// 	return (
 	// 		<SafeAreaView style={styles.safeArea}>
 	// 			<StatusBar barStyle='light-content' backgroundColor={PRIMARY[500]} animated={true} />
-	// 			<CronchyUser user={MyMICDS.auth.snapshot ? MyMICDS.auth.snapshot.user : null} />
+	// 			<View style={styles.userInfo}>
+	// 				<Text style={typography.h3}>Logged in as: {MyMICDS.auth.snapshot ? MyMICDS.auth.snapshot.user : ''}</Text>
+	// 				<Button
+	// 					title='Logout'
+	// 					onPress={this.logout}
+	// 					buttonStyle={components.buttonStyle}
+	// 					titleStyle={components.buttonText}
+	// 				/>
+	// 			</View>
+	//
+	// 			<Text style={typography.h1}>Profile Settings</Text>
+	//
+	// 			<Formik
+	// 				initialValues={settingsFormInitialValues}
+	// 				onSubmit={this.saveSettings}
+	// 			>
+	// 				{props => (
+	// 					<View style={styles.container}>
+	//
+	// 						<Question
+	// 							question='How should we handle your timer data?'
+	// 							responses={this.questionNames}
+	// 							onSelectResponse={handleFieldChangeFactory<SettingsFormValues>(props, 'dataSharingSelection')}
+	// 							selectedIndex={props.values.dataSharingSelection}
+	// 						/>
+	// 						<View style={styles.alarmPicker}>
+	// 							<Text style={[typography.h3, styles.alarmPickerLabel]}>Alarm Preferences</Text>
+	// 							<Picker
+	// 								selectedValue={props.values.alarmSelection}
+	// 								onValueChange={handleFieldChangeFactory<SettingsFormValues>(props, 'alarmSelection')}
+	// 							>
+	// 								{alarmList.map((alarm, i) =>
+	// 									<Picker.Item
+	// 										key={i}
+	// 										label={alarm.displayName}
+	// 										value={i}
+	// 									/>
+	// 								)}
+	// 							</Picker>
+	// 						</View>
+	//
+	// 						<Button
+	// 							title='Save'
+	// 							onPress={props.handleSubmit as any}
+	// 							buttonStyle={components.buttonStyle}
+	// 							titleStyle={components.buttonText}
+	// 						/>
+	// 					</View>
+	// 				)}
+	// 			</Formik>
+	// 			{/* Logout button for debug */}
+	// 			<Button
+	// 				title='Logout'
+	// 				onPress={this.logout}
+	// 				buttonStyle={components.buttonStyle}
+	// 				titleStyle={components.buttonText}
+	// 			/>
 	// 		</SafeAreaView>
 	// 	);
 	// }
 
+	render() {
+		return (
+			<SafeAreaView style={styles.safeArea}>
+				<StatusBar barStyle='light-content' backgroundColor={PRIMARY[500]} animated={true} />
+				<CronchyUser style={styles.avatar} user={MyMICDS.auth.snapshot ? MyMICDS.auth.snapshot.user : null} />
+				<View style={styles.loggedInAsContainer}>
+					<Text style={[typography.body, styles.loggedInAs]}>Logged in as</Text>
+					<Text style={[typography.h0, styles.loggedInUser]}>{MyMICDS.auth.snapshot ? MyMICDS.auth.snapshot.user : ''}</Text>
+				</View>
+				<Button title='Logout' onPress={this.logout} buttonStyle={components.buttonStyle} titleStyle={components.buttonText} />
+				{/*<Formik
+					initialValues={settingsFormInitialValues}
+					onSubmit={this.saveSettings}
+				>
+					{props => (
+						<View>
+							<Text>Alarm Sound</Text>
+							<Question
+								question='Alarm Sound'
+								responses={alarmList.map(i => i.displayName)}
+								onSelectResponse={props.handleChange.}
+								selectedIndex={props.values.dataSharingSelection}
+							/>
+						</View>
+					)}
+				</Formik>*/}
+			</SafeAreaView>
+		);
+	}
+
 }
 
 const styles = StyleSheet.create({
+	safeArea: {
+		height: '100%',
+		padding: 8,
+		display: 'flex',
+		alignItems: 'center'
+	},
+	avatar: {
+		width: '50%'
+	},
+	loggedInAsContainer: {
+		marginBottom: 16
+	},
+	loggedInAs: {
+		textAlign: 'center',
+		color: NEUTRAL[700]
+	},
+	loggedInUser: {
+		textAlign: 'center',
+		lineHeight: 58
+	}
+});
+
+const oldStyles = StyleSheet.create({
 	safeArea: {
 		height: '100%'
 	},
