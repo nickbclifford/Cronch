03705b813c7bf76222cffd697505d2189bf8975b
stlgolfer <amigala@micds.org>
2019-02-23 02:41:11 -0600

    Adding more to stats-lite dec

--- expo/custom-types/stats-lite.d.ts
+++ expo/custom-types/stats-lite.d.ts
@@ -1,4 +1,6 @@
 declare module 'stats-lite' {
+	export function numbers(arr: string[] | number[]): number[];
+	export function sum(arr: string[] | number[]): number[];
     export function stdev(arr: number[]): number;
     export function mean(arr: number[]): number;
 }
